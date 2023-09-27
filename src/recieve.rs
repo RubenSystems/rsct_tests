c70c0383054cc2b856c8b797239355196a0ab04b
RubenSystems <ruben.ticehurst-james@kcl.ac.uk>
2023-09-27 08:55:27 +0100

    update for new rsct

--- src/recieve.rs
+++ src/recieve.rs
@@ -1,18 +1,30 @@
-use tokio::net::UdpSocket;
-use std::io;
+use rsct::{allocators::basic_allocator::BasicAllocator, server::Server};
 
 #[repr(C)]
-pub struct Packet { 
-    index: u8, 
-    client_tied_id: u8, 
-    total: u8 
+pub struct Packet {
+    index: u8,
+    client_tied_id: u8,
+    total: u8,
 }
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
-	let server = rsct::server::Server::new("0.0.0.0", "5254").await;
-	server.start_listener().await;
+async fn main() {
+    let mut server = Server::<BasicAllocator>::new("0.0.0.0", "5254", BasicAllocator).await;
+    loop {
+        let (client, data) = server.recieve_once().await;
 
-	Ok(())
+        let string_val = String::from_utf8_lossy(&data).to_string();
 
+        let ip_string = if let Some(client) = &client {
+            client.ip_string()
+        } else {
+            "unknown".to_string()
+        };
+        let port_string = if let Some(client) = &client {
+            client.port_string()
+        } else {
+            "unknown".to_string()
+        };
+        println!("{ip_string}:{port_string} - {string_val}");
+    }
 }
--- src/transmit.rs
+++ src/transmit.rs
@@ -1,16 +1,16 @@
-use rsct::transmit::transmit;
-use tokio::net::UdpSocket;
-use std::net::SocketAddr;
+use rsct::{allocators::basic_allocator::BasicAllocator, server::Server};
+use std::time::Instant;
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    // Create a UDP socket bound to a specific address and port
-    let socket = UdpSocket::bind("0.0.0.0:5253").await.unwrap();
+fn main() {
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(4)
+        .enable_io()
+        .build()
+        .unwrap();
 
-    // Set a timeout for receiving data (non-blocking with timeout)
-
-	let destination: SocketAddr = "127.0.0.1:5254".parse().unwrap();
-
+    let server = runtime.block_on(async {
+        Server::<BasicAllocator>::new("0.0.0.0", "5253", BasicAllocator).await
+    });
     let string = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque. Enim diam vulputate ut pharetra sit amet aliquam id diam. Sit amet mauris commodo quis imperdiet massa tincidunt nunc. Ac auctor augue mauris augue neque. A scelerisque purus semper eget duis at tellus at. Lectus mauris ultrices eros in cursus turpis. Pretium fusce id velit ut. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Sem fringilla ut morbi tincidunt augue interdum. At erat pellentesque adipiscing commodo elit. Id consectetur purus ut faucibus pulvinar.
 
     Scelerisque in dictum non consectetur a erat nam at. Amet venenatis urna cursus eget nunc scelerisque. Vestibulum morbi blandit cursus risus at ultrices. Turpis tincidunt id aliquet risus. Sit amet volutpat consequat mauris nunc congue nisi vitae suscipit. Ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Pulvinar elementum integer enim neque volutpat ac. Nam aliquam sem et tortor consequat id porta nibh venenatis. Arcu cursus euismod quis viverra. Non enim praesent elementum facilisis leo. In arcu cursus euismod quis viverra nibh. Turpis cursus in hac habitasse platea dictumst quisque. Mauris commodo quis imperdiet massa tincidunt. At volutpat diam ut venenatis tellus in. Augue lacus viverra vitae congue eu consequat. Aliquam sem et tortor consequat id porta nibh venenatis cras. Aliquet nibh praesent tristique magna. Ornare lectus sit amet est placerat in egestas erat. Morbi tristique senectus et netus et malesuada fames ac turpis.
@@ -20,9 +20,14 @@ async fn main() -> std::io::Result<()> {
     Egestas egestas fringilla phasellus faucibus scelerisque. Pretium quam vulputate dignissim suspendisse in est ante. Quam adipiscing vitae proin sagittis nisl rhoncus mattis. Turpis egestas sed tempus urna et pharetra pharetra massa. Quis hendrerit dolor magna eget est lorem ipsum. Mi proin sed libero enim sed. Ipsum a arcu cursus vitae congue mauris. Morbi quis commodo odio aenean. Sagittis vitae et leo duis ut diam quam nulla porttitor. Ut placerat orci nulla pellentesque dignissim enim sit amet. Auctor elit sed vulputate mi sit amet mauris commodo quis. Et malesuada fames ac turpis egestas sed. Ipsum a arcu cursus vitae congue mauris rhoncus.
     
     Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Aliquet lectus proin nibh nisl condimentum id venenatis a. Eu volutpat odio facilisis mauris sit amet massa vitae. Vel turpis nunc eget lorem dolor sed viverra ipsum nunc. Dignissim convallis aenean et tortor at. At volutpat diam ut venenatis. Platea dictumst quisque sagittis purus sit amet volutpat. Enim blandit volutpat maecenas volutpat blandit. Lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique. Quisque id diam vel quam elementum pulvinar etiam non quam. Sed libero enim sed faucibus turpis in eu mi bibendum. Feugiat scelerisque varius morbi enim nunc faucibus. Eget nunc scelerisque viverra mauris. In nisl nisi scelerisque eu.";
+    let cli = rsct::client::Client::from_string("127.0.0.1:5254".to_string());
 
-    transmit(string as *const u8, string.len(), &socket, &destination).await;
+    let now = Instant::now();
+    runtime.block_on(async {
+        server.transmit_concurrently(string, &cli, &runtime).await;
+    });
+    
 
-
-	Ok(())
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
